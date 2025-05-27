c21eec0f13f682bf5cd336a59bb652054614339d
satishkumar <garthimas@gmail.com>
2025-05-27 05:36:54 +0530

    New Model has been created to hold the old and new price of the product.

---
+++ app/src/main/java/com/application/d4candroidassignment/shop/models/Price.kt
@@ -0,0 +1,7 @@
+package com.application.d4candroidassignment.shop.models
+
+// This data class is will give us an idea of any price difference and we can also improve this for adding discounts if any
+data class Price(
+    val oldPrice : String,
+    val newPrice : String
+)
