b42f583780963561a9bd1a04fb6a8dc8009cb11d
satishkumar <garthimas@gmail.com>
2025-05-27 05:37:33 +0530

    Currency model is created in such a way that this can be used for multiple currency

---
+++ app/src/main/java/com/application/d4candroidassignment/shop/models/Currency.kt
@@ -0,0 +1,9 @@
+package com.application.d4candroidassignment.shop.models
+
+// This is small sample data class to handle multiple currency
+data class Currency(
+    val currency : String,
+    val currencyName : String,
+    val currencyCode : String
+)
+
