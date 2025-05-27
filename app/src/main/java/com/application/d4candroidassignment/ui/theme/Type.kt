5fce69c38619cafdf783f7fec846ebaf07b5c350
satishkumar <garthimas@gmail.com>
2025-05-27 05:40:55 +0530

    all the font files has been added to the application and are defined in the Type.kt file for easy access

--- app/src/main/java/com/application/d4candroidassignment/ui/theme/Type.kt
+++ app/src/main/java/com/application/d4candroidassignment/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.application.d4candroidassignment.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.application.d4candroidassignment.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,13 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+val tangerine = FontFamily(
+    Font(R.font.tangerine),
+    Font(R.font.tangerine_bold),
+    Font(R.font.tangerine_regular)
+)
+
+val centuryOldStyle = FontFamily(
+    Font(R.font.century_old_style_std_bold)
 )
